fbd5c6630a26f80a26f6c3b13a8eb6aedda48106
DEPCFEC <64804771+DEPCFEC@users.noreply.github.com>
2020-05-05 15:03:46 +0600

    Update B.c

--- B.c
+++ B.c
@@ -1,13 +1,18 @@
-//Abid Hasan Roni
-#include <stdio.h>
+//Aslam Mahmud Siam
+#include<stdio.h>
+
+int abs(int n){
+    if(n<0)return n*-1;
+    return n;
+}
 int main()
 {
 	int t,cs=1;
 	scanf("%d",&t);
 	while(t--){
-	    int a,b;
-	    scanf("%d%d",&a,&b);
-        printf("Case %d: %d\n",cs++,a);
+	    int a,n;
+	    scanf("%d%d",&a,&n);
+	    if(a!=0)printf("Case %d: %d\n",cs++,abs(a));
+	    else printf("Case %d: %d\n",cs++,n);
 	}
-    return 0;
 }
